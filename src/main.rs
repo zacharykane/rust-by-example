d77ce48acf186f84ffa2176f00f08317836358d0
Zachary Kane Pride <code@zacharykane.net>
2024-02-27 10:54:48 -0800

    try different formatted prints

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,18 @@ impl fmt::Display for Printable {
 }
 
 #[derive(Debug)]
-struct Person<'a> {
-    name: &'a str,
+struct Person {
+    name: String,
     age: u8,
 }
 
-impl fmt::Display for Person<'_> {
+impl fmt::Display for Person {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "name is {}, and age is {}", self.name, self.age)
     }
 }
 
-// Define a structure named `List` containing a `Vec`.
+// Define a structure named `List` containing a `Vec` of 32 bit integers.
 struct List(Vec<i32>);
 
 impl fmt::Display for List {
@@ -54,30 +54,115 @@ impl fmt::Display for List {
     }
 }
 
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let value: u32 =
+            u32::from(self.red) * 65536 + u32::from(self.green) * 256 + u32::from(self.blue);
+
+        let red = self.red;
+        let green = self.green;
+        let blue = self.blue;
+        let hex = format!("{:0>6X}", value);
+
+        write!(f, "RGB ({red}, {green}, {blue}) 0x{hex}")
+    }
+}
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Matrix(a, b, c, d) = self;
+
+        write!(f, "( {a} {b} )\n( {c} {d} )")
+    }
+}
+
+fn transpose(pair: Matrix) -> Matrix {
+    let Matrix(a, b, c, d) = pair;
+    Matrix(a, c, b, d)
+}
+
 fn main() {
-    println!("Hello, {world:>10}!", world = "world");
+    println!("Hello, {world:>10}!", world = "world, padded with spaces");
 
     let x = "x";
-    println!("Hello {x} is {pi:.2}", pi = PI);
+    println!(
+        "Hello {x} is {pi:.2}, set to 2 significant figures",
+        pi = PI
+    );
 
     let debug_printable = DebugPrintable(123);
-    println!("{:?}", debug_printable);
+    println!("Debug printable: {:?}", debug_printable);
 
     let deep_printable = Deep(debug_printable);
-    println!("{:?}", deep_printable);
-    println!("{:#?}", deep_printable);
+    println!("Nested Debug printable: {:?}", deep_printable);
+    println!("Nested Debug printable (multiline): {:#?}", deep_printable);
 
     let display_printable = Printable(64);
-    println!("{}", display_printable);
+    println!(
+        "Display printable (see Struct Impl above) {}",
+        display_printable
+    );
 
     let pretty_person = Person {
-        name: "Zach",
+        name: String::from("Zach"),
         age: 39,
     };
-    println!("{:?}", pretty_person);
-    println!("{:#?}", pretty_person);
-    println!("{}", pretty_person);
+    println!("Debug printable: {:?}", pretty_person);
+    println!("Debug printable (multiline): {:#?}", pretty_person);
+    println!("Display printable (custom Impl): {}", pretty_person);
 
     let list = List(vec![1, 2, 3]);
-    println!("{}", list);
+    println!("Display printable Struct w/ Vector: {}", list);
+
+    println!("Display printable in for loop:");
+    for color in [
+        Color {
+            red: 128,
+            green: 255,
+            blue: 90,
+        },
+        Color {
+            red: 0,
+            green: 3,
+            blue: 254,
+        },
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0,
+        },
+    ] {
+        println!("{}", color);
+    }
+
+    let long_tup = (
+        1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true,
+    );
+
+    println!(
+        "Display printable primitive from Tuple: {}, {}, {}",
+        long_tup.0, long_tup.5, long_tup.10
+    );
+
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+    println!("Debug, tuple of tuples: {:?}", tuple_of_tuples);
+
+    let tuple = (1, "hello", 4.5, true);
+    let (a, b, c, d) = tuple;
+    println!("Debug deconstructed: {:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("Debug printable {:?}", matrix);
+    println!("Display Matrix:\n{}", matrix);
+    println!("Display Transposed Matrix:\n{}", transpose(matrix))
 }
